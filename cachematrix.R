16a08912f49187a1c239f443921c62aaad53de6d
pabloabc <pablou25@gmail.com>
2014-05-25 01:40:53 -0300

    commit para grabar cambios locales en proy ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## This function creates a special "matrix" object that can cache its inverse.
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setMinv <- function(Minv) m <<- Minv
+    getMinv <- function() m
+    list(set = set, get = get,
+         setMinv = setMinv,
+         getMinv = getMinv)    
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getMinv()
+    if(!is.null(m)) {
+        message(paste("Getting Matrix Inverse stored previously in cache.Using cached data.",
+                      "Nothing computed here.", sep="") )
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setMinv(m)
+    m    
 }
